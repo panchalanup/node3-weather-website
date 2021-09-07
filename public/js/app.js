eaf9f2701b7f39e561233caddc42498d2adf2a03
panchalanup <anupsawesomeapple@gmail.com>
2021-09-07 13:58:42 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ weatherForm.addEventListener('submit', (event) => {
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = ''
 
-        fetch(`http://localhost:3000/weather?address=${location}`)
+        fetch(`/weather?address=${location}`)
             .then((response) => {
                 response.json().then((data) => {
                     if (data.error) {
--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+// For Heroku server 
+const port = process.env.PORT || 3000
 
 //console.log(path.join(__dirname,'../public'))
  
@@ -106,8 +108,12 @@ app.get('*',(req,res) => {
     })
 })
 
-app.listen(3000,() => {
-    console.log('Server is up on port 3000')
-})
+// app.listen(3000,() => {
+//     console.log('Server is up on port 3000')
+// })
 
-// ?search=gaming&rating=5  
+//  For Heroku server
+
+app.listen(port,() => {
+    console.log('Server is up on port ' + port)
+})
